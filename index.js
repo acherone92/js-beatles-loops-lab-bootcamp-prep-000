51775353dd5a07ee134d029524487923d8533966
acherone92@gmail.com <acherone92@gmail.com>
2018-03-20 15:58:46 +0000

    automatically backed up by learn

---
+++ index.js
@@ -0,0 +1,21 @@
+function theBeatlesPlay(musicians, instruments){
+  var beatleIDs = [];
+  for (let i = 0; i<musicians.length; i++){
+    beatleIDs[i] = musicians[i] + " plays " + instruments[i];
+  }
+  console.log(beatleIDs);
+  return beatleIDs;
+}
+
+function johnLennonFacts(facts){
+  var loudFacts = [];
+  while (loudFacts.length < facts.length){
+    var i = 0;
+    loudFacts[i] = facts[i] + "!!!";
+    console.log(loudFacts); 
+    i++;
+  }
+  return loudFacts;
+}
+
+test
